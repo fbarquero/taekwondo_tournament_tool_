9f9acb40ad93fa731655226e96daba8f3cbf028c
fbarquero <fbarquero86@gmail.com>
2014-05-20 23:48:23 -0600

    Updates: is_power_of_two get_exponent_base_two

--- takeBreakModule/single_elimination_bracket.py
+++ takeBreakModule/single_elimination_bracket.py
@@ -21,27 +21,39 @@ def shuffle_players(players):
     return players
 
 def match_maker(shuffled_players):
+    # Object that will represents the bracket
+    bracket = []
+    # Represents the round matches
     matches = []
+    # Number of matches in the first round
+    byes = 0
+    # Number of rounds in the bracket
+    rounds = 0
+
     count = 0
     print "shuffled players:"
     print shuffled_players
-    if len(shuffled_players) % 2 == 1:
-        print "impar"
-        matches.append(shuffled_players[count])
-        count = 1
-        while count != (len(shuffled_players)/2) + 1:
-                matches.append([shuffled_players[count],shuffled_players[(len(shuffled_players))-count]])
-                count += 1
-    else:
-        print "par"
-        while count != len(shuffled_players)/2:
+    if is_power_of_two(len(shuffled_players)):
+       while count != len(shuffled_players)/2:
                 matches.append([shuffled_players[count],shuffled_players[(len(shuffled_players)-1)-count]])
                 count += 1
+    else:
+        print "needs to calculate byes"
     print "matches:"
     print matches
     return matches
 
+def is_power_of_two(number):
+    return (number & (number-1) == 0) and (number != 0)
 
-single_elimination_bracket_generation(["alonso","darian","michael","will", "papi"])
+def get_exponent_base_two(number):
+    exponent = 0
+    while number > 1:
+        number /= 2
+        exponent += 1
+    return exponent
+
+#single_elimination_bracket_generation(["alonso", "darian", "michael", "lunita"])
+print get_exponent_base_two(64)
 
 
