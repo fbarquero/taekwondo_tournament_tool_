ea7ed003a96dfe249aa16edcb1079ab75005038e
fbarquero <fbarquero86@gmail.com>
2014-05-22 17:37:18 -0600

    Update listing byed players, removing it from shuffled players list

--- takeBreakModule/single_elimination_bracket.py
+++ takeBreakModule/single_elimination_bracket.py
@@ -8,8 +8,6 @@ def single_elimination_bracket_generation(players):
 
 
 def shuffle_players(players):
-    print "players :"
-    print players
     amount_of_players = len(players) - 1
     while amount_of_players != 0:
         random_generated = random.randint(0, amount_of_players)
@@ -17,8 +15,6 @@ def shuffle_players(players):
         players[random_generated] = players[amount_of_players]
         players[amount_of_players] = aux_player
         amount_of_players -= 1
-    print "Shuffled list"
-    print players
     return players
 
 
@@ -32,44 +28,54 @@ def match_maker(shuffled_players):
     count = 0
     print "shuffled players:"
     print shuffled_players
+    byed_players = [None]
 
     if is_power_of_two(len(shuffled_players)):
         while count != len(shuffled_players) / 2:
             matches.append([shuffled_players[count], shuffled_players[(len(shuffled_players) - 1) - count]])
             count += 1
         bracket.append(matches)
-        bracket = generate_next_rounds(len(shuffled_players), bracket)
     else:
         byes = get_byes(len(shuffled_players))
-        byed_players = []
         random_used = []
         count = 0
         while count < byes:
-            random_used.append(random.randint(0, len(shuffled_players)-1))
-            while 
-            byed_players.append(shuffled_players[ram])
+            while True:
+                rand_num = random.randint(0, len(shuffled_players)-1)
+                if not rand_num in random_used:
+                    random_used.append(rand_num)
+                    break
+            byed_players.append(shuffled_players[random_used[count]])
+            shuffled_players.remove(byed_players[count])
             count += 1
-        print "needs to calculate byes"
+        print "Byed Players:"
+        print byed_players
+        print "shufled players after bye process:"
+        print shuffled_players
+    bracket = generate_next_rounds(len(shuffled_players), bracket, byed_players)
     bracket.append([None])
     print "bracket:"
     print bracket
     return bracket
 
-def generate_next_rounds(number_of_players, bracket):
+def generate_next_rounds(number_of_players, bracket, byed_players):
     # Number of rounds in the bracket
     rounds = get_exponent_base_two(number_of_players)
     count = 0
     matches = []
-    matches_next_round = number_of_players/4
-    while rounds > 1:
-        while matches_next_round > count:
-            matches.append([None]*2)
-            count += 1
-        matches_next_round /= 2
-        bracket.append(matches)
-        matches = []
-        rounds -= 1
-        count = 0
+    if None in byed_players:
+        matches_next_round = number_of_players/4
+        while rounds > 1:
+            while matches_next_round > count:
+                matches.append([None]*2)
+                count += 1
+            matches_next_round /= 2
+            bracket.append(matches)
+            matches = []
+            rounds -= 1
+            count = 0
+    else:
+        print "Next roudns for byed players"
     return bracket
 
 def is_power_of_two(number):
@@ -93,7 +99,7 @@ def get_exponent_base_two(number):
     return exponent
 
 #single_elimination_bracket_generation(["alonso", "darian", "michael", "lunita", "5", "6", "7", "8", "alonso2", "darian2", "michael2", "lunita2", "5_2", "6_2", "7_2", "8_2"])
-
+single_elimination_bracket_generation(["alonso", "darian", "michael", "jack"])
 
 
 
