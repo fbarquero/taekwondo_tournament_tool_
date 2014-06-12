27501a8380a087854012dc2b5914cee7a96324da
fbarquero <fbarquero86@gmail.com>
2014-06-12 07:59:53 -0600

    Merge branch 'master' of https://github.com/fbarquero/taekwondo_tournament_tool
    Conflicts:
    	bracket/bracket.html
    	takeBreakModule/single_elimination_bracket.py

--- takeBreakModule/single_elimination_bracket.py
+++ takeBreakModule/single_elimination_bracket.py
@@ -1,6 +1,7 @@
 __author__ = 'Mordigan'
 import random
 import math
+import uuid
 
 
 def single_elimination_bracket_generation(players):
@@ -54,6 +55,7 @@ def match_maker(shuffled_players):
                 if count < len(byed_players):
                     matches['teams'].append([byed_players[count], '-- -- --'])
                     matches['results'].append([0, -1])
+                    count += 1
                 matches['teams'].append([shuffled_players[count], shuffled_players[(len(shuffled_players) - 1) - count]])
                 matches['results'].append([0, 0])
                 count += 1
@@ -61,13 +63,42 @@ def match_maker(shuffled_players):
             while count != next_higher_power_of_two / 2:
                 matches['teams'].append([byed_players[count], '-- -- --'])
                 matches['results'].append([0, -1])
-                if count < len(shuffled_players):
+                if count < len(shuffled_players)/2:
                     matches['teams'].append([shuffled_players[count], shuffled_players[(len(shuffled_players) - 1) - count]])
                     matches['results'].append([0, 0])
+                    count += 1
                 count += 1
+        #bracket.append(matches)
+        #print "Byed Players:"
+        #print byed_players
+        #print "shufled players after bye process:"
+        #print shuffled_players
+    #bracket = generate_next_rounds(len(shuffled_players), bracket, byed_players)
+    #bracket.append([None])
     print "Matches dict:"
     print matches
     return matches
+
+def generate_next_rounds(number_of_players, bracket, byed_players):
+    count = 0
+    matches = []
+    if len(byed_players) == 0:
+        rounds = get_exponent_base_two(number_of_players)
+        matches_next_round = number_of_players/4
+        while rounds > 1:
+            while matches_next_round > count:
+                matches.append([None]*2)
+                count += 1
+            matches_next_round /= 2
+            bracket.append(matches)
+            matches = []
+            rounds -= 1
+            count = 0
+    else:
+        #TODO work on generate next rounds for byes
+
+        print ""
+    return bracket
 
 def is_power_of_two(number):
     if number == 0:
@@ -93,6 +124,10 @@ def get_exponent_base_two(number):
 #single_elimination_bracket_generation(["alonso", "darian", "michael", "lunita", "5", "6", "7", "8", "alonso2", "darian2", "michael2", "lunita2", "5_2", "6_2", "7_2", "8_2"])
 #single_elimination_bracket_generation(["alonso", "darian", "michael", "josue", "lucia", "servet tezagul"])
 #single_elimination_bracket_generation(["alonso", "michael", "aaron cook"])
+#single_elimination_bracket_generation(["Aroon cook", "Tezaqul", "Tuncat", "Steven L", "nikpah"])
+#single_elimination_bracket_generation(["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15","16"])
+single_elimination_bracket_generation(["1", "2", "3", "4", "5", "6"])
+print hex(uuid.getnode())
 
 
 
